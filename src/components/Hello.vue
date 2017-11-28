bf396a26d0ecfc6bb644cdd9fc7eff49ad9e0b46
Lily <lily.bickerstaffe@live.co.uk>
2017-11-28 21:51:47 +0700

    Added moving snake on keydown

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -19,6 +19,11 @@ export default {
       snake: [],
       deltaFrame: 0,
       targetFPS: 1,
+      left: 37,
+      up: 38,
+      right: 39,
+      down: 40,
+      direction: 0
     }
   },
   methods: {
@@ -29,6 +34,10 @@ export default {
     draw () {
       this.ctx.fillStyle = 'white'
       this.ctx.fillRect(0, 0, 500, 500)
+
+      this.ctx.fillStyle = '#cc0000'
+      this.ctx.fillRect(150, 150, 10, 10)
+
       this.ctx.fillStyle = '#99ffbb'
 
       for (let i = 0; i < this.snake.length; i++) {
@@ -37,30 +46,48 @@ export default {
     },
     update () {
       let head = []
-      head[0] = this.snake[0][0] + 10
+      head[0] = this.snake[0][0]
       head[1] = this.snake[0][1]
+
+      if (this.direction === this.left) {
+        head[0] -= 10
+      } else if (this.direction === this.right) {
+        head[0] += 10
+      } else if (this.direction === this.up) {
+        head[1] -= 10
+      } else if (this.direction === this.down) {
+        head[1] += 10
+      }
 
       this.snake.pop()
       this.snake.unshift(head)
+    },
+    handleKeyDown (event) {
+      if (event.keyCode >= 37 && event.keyCode <= 40) {
+        event.preventDefault()
+        this.direction = event.keyCode
+      }
     }
   },
   created () {
     this.deltaFrame = 1000 / this.targetFPS
-    this.snake = [[30, 10], [30, 10], [20, 10], [10, 10]]
-    let started = false
+    this.snake = [[40, 10], [30, 10], [20, 10], [10, 10]]
+    this.direction = this.right
 
     Vue.nextTick(() => {
       this.canvas = this.$refs.canvas
       this.ctx = this.canvas.getContext('2d')
-      this.ctx.fillStyle = '#cc0000'
-      this.ctx.fillRect(150, 150, 10, 10)
+      this.draw()
 
       // BUTTON
-      if(!started) {
-        started = true
-        setInterval(this.gameLoop, this.deltaFrame)
-      }
+      setInterval(this.gameLoop, this.deltaFrame)
     })
+  },
+  mounted () {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this))
+  },
+  beforeDestroy () {
+    window.removeEventListener('keydown', this.handleKeyDown.bind(this))
   }
 }
 </script>
